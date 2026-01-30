0575582107ddfab7218a2edfd58f84a71f05650c
vu.nguyen <camping89@gmail.com>
2026-01-30 16:39:25 +0700

    adds request logging middleware

--- src/common/tracing/__init__.py
+++ src/common/tracing/__init__.py
@@ -2,5 +2,11 @@
 
 from src.common.tracing.context import get_correlation_id, set_correlation_id
 from src.common.tracing.correlation import CorrelationIDMiddleware
+from src.common.tracing.request_logging import RequestLoggingMiddleware
 
-__all__ = ["get_correlation_id", "set_correlation_id", "CorrelationIDMiddleware"]
+__all__ = [
+    "get_correlation_id",
+    "set_correlation_id",
+    "CorrelationIDMiddleware",
+    "RequestLoggingMiddleware",
+]
